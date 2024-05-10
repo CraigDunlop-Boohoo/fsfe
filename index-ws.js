c965b3fc2147888bd05a9e4d580c01901bd3ddf4
Craig Dunlop <craig.dunlop@boohoo.com>
2024-05-10 10:41:51 +0100

    copied from repo

--- index-ws.js
+++ index-ws.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const server = require("http").createServer();
 const app = express();
+const PORT = 3000;
 
 app.get("/", function (req, res) {
   res.sendFile("index.html", { root: __dirname });
@@ -8,33 +9,45 @@ app.get("/", function (req, res) {
 
 server.on("request", app);
 
-server.listen(3000, function () {
-  console.log("Server started on port 3000");
+server.listen(PORT, function () {
+  console.log("Listening on " + PORT);
 });
 
-/** Begin websocket */
+/** Websocket **/
 const WebSocketServer = require("ws").Server;
 
 const wss = new WebSocketServer({ server: server });
 
 wss.on("connection", function connection(ws) {
   const numClients = wss.clients.size;
-  console.log("Clients connected: ", numClients);
+
+  console.log("clients connected: ", numClients);
 
   wss.broadcast(`Current visitors: ${numClients}`);
 
   if (ws.readyState === ws.OPEN) {
-    ws.send("Welcome to my server");
+    ws.send("welcome!");
   }
 
   ws.on("close", function close() {
-    wss.broadcast(`Current visitors: ${numClients}`);
+    wss.broadcast(`Current visitors: ${wss.clients.size}`);
     console.log("A client has disconnected");
+  });
+
+  ws.on("error", function error() {
+    //
   });
 });
 
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
 wss.broadcast = function broadcast(data) {
+  console.log("Broadcasting: ", data);
   wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
+/** End Websocket **/
